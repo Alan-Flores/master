8564cff696c0ce3371f6f9c79a782c939bca00b2
Alan-Flores <alanflores@hotmail.cl>
2022-08-17 20:10:24 -0400

    deploy page

--- deploy.sh
+++ deploy.sh
@@ -52,6 +52,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:alan-flores/matriz.git main:gh-pages
+# git push -f git@github.com:alan-flores/repo-root.git main:gh-pages
 
 cd -
