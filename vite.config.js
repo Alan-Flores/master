42daeed09b979b76e39cb559eabd3940b0686a4e
Alan-Flores <alanflores@hotmail.cl>
2022-08-19 14:56:44 -0400

    up-page

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   /* The base path for the project. */
-  //base: '/repo-root/'
+  base: '/repo-root/'
 })
