2bd2ffbec17c92c5f1665a614b18060ef4905567
Alan-Flores <alanflores@hotmail.cl>
2023-04-15 23:19:28 -0400

    Portada de actualizacion de servicios

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
           <img src={circuloCS} className="circle" alt="Consof logo" />         
         </a>
       </div>
-      <h1 className="mt-5 mb-4">Matriz de Proyectos</h1>
-      <span>Plantilla de proyectos</span>
+      <h1 className="mt-5 mb-4">Servicio en mantenimiento</h1>
+      <span>Estamos trabajando para mejorar sus servicios.</span>
     </div>
   )
 }
