0c69de4bdcb55ece6ebf97f41f992926f1e6dbbe
Alan-Flores <alanflores@hotmail.cl>
2023-05-01 04:51:23 -0400

    Botones de login provisorios farmacia de lo alto

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import consofLogo from './assets/img/consofCS.svg';
 import circuloCS from './assets/img/circuloCS.svg';
+import { ButtonLocal } from './assets/img/ButtonLocal';
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
+      <ButtonLocal />
       <div>
         <a></a>
+        <h2 className="mb-4">Pagina en construcción</h2>
         <a href="https://www.consofchile.cl/" target="_blank">
           <img src={consofLogo} className="logo" alt="Consof logo" />
           <img src={circuloCS} className="circle" alt="Consof logo" />         
         </a>
       </div>
-      <h1 className="mt-5 mb-4">Matriz de Proyectos</h1>
-      <span>Plantilla de proyectos</span>
     </div>
   )
 }
---
+++ src/assets/img/ButtonLocal.jsx
@@ -0,0 +1,16 @@
+
+
+export const ButtonLocal = () => {
+    return (
+        <>
+        <h1 className="mb-3">Selecciona tu local:</h1>
+            <div className="mb-5">
+                <a href="https://sancarlos.deloalto.cl/" type="button" className="btn btn-primary btn-lg m-2">San Carlos</a>
+                <a href="https://ejercito.deloalto.cl/" type="button" className="btn btn-secondary btn-lg m-2">Ejercito</a>
+                <a href="https://creta.deloalto.cl/" type="button" className="btn btn-success btn-lg m-2">Creta</a>
+                <a href="https://lapintana.deloalto.cl/" type="button" className="btn btn-danger btn-lg m-2">La Pintana</a>
+                <a href="https://lacisterna.deloalto.cl/" type="button" className="btn btn-warning btn-lg m-2">La Cisterna</a>
+            </div>
+        </>
+    )
+}
