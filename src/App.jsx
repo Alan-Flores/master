3e84def713b0d0e12edda5b87dd1f55b963e2369
Alan Flores <alanflores@hotmail.cl>
2024-06-25 20:35:54 -0400

    Se agrega boton para direccionar al TPV

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,24 @@ function App() {
         <a></a>
         <a href="https://www.consofchile.cl/" target="_blank">
           <img src={consofLogo} className="logo" alt="Consof logo" />
-          <img src={circuloCS} className="circle" alt="Consof logo" />         
+          <img src={circuloCS} className="circle" alt="Consof logo" />
         </a>
       </div>
-      <h1 className="mt-5 mb-4">Matriz de Proyectos</h1>
-      <span>Plantilla de proyectos</span>
+      <h1 className="mt-5 mb-4">Terminal de punto de Ventas</h1>
+      <span>Consof Ingenieria</span>
+      <div>
+        <a href='https://consof.net'>
+          <button 
+            type='button' 
+            className='btn btn-primary btn-lg mt-5' 
+            onclick="window.location='https://consof.net'"
+            role='link'>
+
+            Ir al TPV
+
+          </button>
+          </a>
+      </div>
     </div>
   )
 }
